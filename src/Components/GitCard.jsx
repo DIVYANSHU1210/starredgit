b1942ee798fe8361949ca3337f56d7ee3d1e9641
Divyanshu Joshi <divyanshujoshi1012@gmail.com>
2024-02-24 15:58:49 +0530

    aadfa

--- src/Components/GitCard.jsx
+++ src/Components/GitCard.jsx
@@ -13,7 +13,7 @@ function GitCard({repoName, img, desc, stars, issues, pushedAt, ownerName}) {
   const [drop, setDrop] = useState(false);
 
   // token works as access key to get repo's data like commit, addition, deletion history etc.
-  const token = "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";
+  const token = "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";
 
 
   // this function will convert the "pushedAt" date (format - 2019-04-24T07:45:14Z)  into humanly redable date (format-Apr 24, 2019, 01:15:14 PM).
